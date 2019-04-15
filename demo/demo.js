10e3e7fabeb4d14b836f5391464f5cd09e13321f
admin <admin@admindeMacBook-Pro.local>
2019-04-15 18:13:36 +0800

    添加各种帧动画的demo

--- demo/demo.js
+++ demo/demo.js
@@ -11,14 +11,74 @@ var $rabbit1 = $('rabbit1');
 var $rabbit2 = $('rabbit2');
 var $rabbit3 = $('rabbit3');
 var $rabbit4 = $('rabbit4');
+var $egg1 = $('egg1');
+var $egg = $('egg');
+var $loin = $('loin');
+var $xu = $('xu');
+var $star = $('star');
 
-var images = ['rabbit-big.png', 'rabbit-lose.png', 'rabbit-win.png']
+var imagesTmp = ['rabbit-big.png', 'rabbit-lose.png', 'rabbit-win.png', 'egg-c.png', 'egg.png', 'loin.png', 'xu.png']
+
+var starImages = []
+Array(34).fill(0).forEach((item, index) => {
+    starImages.push(`us/${index + 1}.png`)
+})
+
+var images = imagesTmp.concat(starImages)
 
 // var imgUrl = 'rabbit-big.png';
 var rightRunningMap = ["0 -854", "-174 -852", "-349 -852", "-524 -852", "-698 -851", "-873 -848"];
 var leftRunningMap = ["0 -373", "-175 -376", "-350 -377", "-524 -377", "-699 -377", "-873 -379"];
 var rabbitWinMap = ["0 0", "-198 0", "-401 0", "-609 0", "-816 0", "0 -96", "-208 -97", "-415 -97", "-623 -97", "-831 -97", "0 -203", "-207 -203", "-415 -203", "-623 -203", "-831 -203", "0 -307", "-206 -307", "-414 -307", "-623 -307"];
 var rabbitLoseMap = ["0 0", "-163 0", "-327 0", "-491 0", "-655 0", "-819 0", "0 -135", "-166 -135", "-333 -135", "-500 -135", "-668 -135", "-835 -135", "0 -262"];
+
+var loinStyle = [195, 712]
+var loinMap = []
+Array(4).fill(0).forEach((item, index) => {
+    var left = Math.floor(index * (loinStyle[1] / 4))
+    loinMap.push(`-${left} 0`)
+});
+
+var egg1Style = [336, 6894]
+var egg1Map = []
+Array(18).fill(0).forEach((item, index) => {
+    var left = Math.floor(index * (egg1Style[1] * (195 / 336) / 18))
+    egg1Map.push(`-${left} 0`)
+})
+
+var eggStyle = [336, 3064]
+var eggMap = []
+Array(8).fill(0).forEach((item, index) => {
+    var left = Math.floor(index * (eggStyle[1] * (195 / 336) / 8))
+    eggMap.push(`-${left} 0`)
+})
+
+var xuStyle = [195, 712]
+var xuMap = []
+Array(4).fill(0).forEach((item, index) => {
+    var left = Math.floor(index * (xuStyle[1] / 4))
+    xuMap.push(`-${left} 0`)
+})
+
+// (function() {
+//     var loinAn = animation().loadImage([images]).changePosition($loin, loinMap, images[5]).repeatForever();
+//     loinAn.start(100);
+// })();
+
+// (function() {
+//     var egg1An = animation().loadImage([images]).changePosition($egg1, egg1Map, images[3]).repeatForever();
+//     egg1An.start(80);
+// })();
+
+// (function() {
+//     var eggAn = animation().loadImage([images]).changePosition($egg, eggMap, images[4]).repeatForever();
+//     eggAn.start(80);
+// })();
+
+// (function() {
+//     var xuAn = animation().loadImage([images]).changePosition($xu, xuMap, images[6]).repeatForever();
+//     xuAn.start(80);
+// })();
 
 var animation = window.animation;
 
@@ -28,16 +88,12 @@ function repeat() {
     repeatAnimation.start(80);
 }
 
-repeat();
-
 function win() {
     var winAnimation = animation().loadImage([images]).changePosition($rabbit3, rabbitWinMap, images[2]).repeat(3).then(function() {
         console.log('win animation repeat 3 times and fined');
     });
     winAnimation.start(200);
 }
-
-win();
 
 function lose() {
     var loseAnimation = animation().loadImage([images]).changePosition($rabbit4, rabbitLoseMap, images[1]).wait(3000).repeat(1).then(function() {
@@ -46,7 +102,6 @@ function lose() {
     loseAnimation.start(200);
 }
 
-lose();
 
 function run() {
     var speed = 6;
@@ -96,7 +151,33 @@ function run() {
     runAnimation.start(interval);
 }
 
-run();
+// repeat();
+// win();
+// lose();
+// run();
+
+var starStyle = [1187, 1080]
+
+
+function uskidStar() {
+    var frameLength = 33;
+    var frame = 1;
+
+    var starAn = animation().loadImage([images], function()).enterFrame(function(success, time) {
+        console.log('time', frame)
+
+        $star.style.backgroundImage = `url(${images[frame + 7]})`
+        frame++;
+        if (frame > frameLength) {
+            frame = 0;
+            success();
+            return;
+        }
+    }).repeatForever()
+    starAn.start(120)
+}
+
+uskidStar()
 
 
 
